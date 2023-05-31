c1e70241678c612d203300ee2782ba8ecff1397c
Paula Domingues <pdomingues@itba.edu.ar>
2023-05-31 00:59:03 -0300

    feat: load geographic data

---
+++ sql/ba_geom.sql
@@ -0,0 +1,101 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+DROP TABLE IF EXISTS communes, neighborhoods, police_stations, subway_stations;
+DROP TABLE IF EXISTS  temp_police_stations, temp_subway_stations;
+-- COMMUNES ------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS communes(
+    multipolygon geometry,
+    id numeric,
+    object text,
+    commune numeric primary key,
+    neighborhoods text,
+    perimeter numeric,
+    area numeric
+);
+
+COPY communes
+FROM '/Users/paudomingues/Documents/ITBA/OLAP/SecurityInsightsBA-OLAP/data/geo/comunas.csv'
+DELIMITER ';'
+NULL AS ''
+CSV HEADER;
+
+ALTER TABLE communes
+DROP COLUMN id,
+DROP COLUMN neighborhoods,
+DROP COLUMN object;
+
+-- NEIGHBORHOOD ------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS neighborhoods(
+    multipolygon geometry,
+    neighborhood text primary key,
+    commune numeric references communes(commune),
+    perimeter numeric,
+    area numeric,
+    object text
+);
+COPY neighborhoods
+FROM '/Users/paudomingues/Documents/ITBA/OLAP/SecurityInsightsBA-OLAP/data/geo/barrios.csv'
+DELIMITER ';'
+NULL AS ''
+CSV HEADER;
+
+ALTER TABLE neighborhoods
+DROP COLUMN object;
+
+-- SUBWAY STATIONS ------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS temp_subway_stations (
+    id numeric primary key ,
+    long numeric,
+    lat numeric,
+    station text,
+    line text
+);
+
+CREATE TABLE IF NOT EXISTS subway_stations (
+    id serial primary key,
+    coordinates geometry(POINT,0),
+    station text,
+    line text
+);
+
+COPY temp_subway_stations (long, lat, id, station, line)
+FROM '/Users/paudomingues/Documents/ITBA/OLAP/SecurityInsightsBA-OLAP/data/geo/estaciones-de-subte.csv'
+DELIMITER ','
+CSV HEADER;
+
+INSERT INTO subway_stations(coordinates, station, line) SELECT ST_POINT(t.long, t.lat), t.station, t.line FROM temp_subway_stations t;
+DROP TABLE IF EXISTS temp_subway_stations;
+
+-- POLICE STATIONS ------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS temp_police_stations (
+    long numeric,
+    lat numeric,
+    id numeric primary key,
+    nombre text,
+    calle text,
+    altura text,
+    calle2 text,
+    direccion text,
+    telefonos text,
+    observaciones text,
+    observaciones_2 text,
+    barrio text,
+    comuna text,
+    codigo_postal text,
+    codigo_postal_argentino text
+);
+
+CREATE TABLE IF NOT EXISTS police_stations (
+    name text primary key,
+    coordinates geometry(POINT,0),
+    neighborhood text references neighborhoods(neighborhood)
+);
+
+COPY temp_police_stations
+FROM '/Users/paudomingues/Documents/ITBA/OLAP/SecurityInsightsBA-OLAP/data/geo/comisarias-ciudad.csv'
+DELIMITER ','
+NULL AS ''
+CSV HEADER;
+
+INSERT INTO police_stations(name, coordinates, neighborhood) SELECT t.nombre, ST_POINT(t.long, t.lat), t.barrio  FROM temp_police_stations t;
+DROP TABLE IF EXISTS temp_police_stations;
