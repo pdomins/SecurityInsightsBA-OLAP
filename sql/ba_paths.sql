1a0b86bc8f800b250cdf3b20a1f27c497b4f9094
Paula Domingues <pdomingues@itba.edu.ar>
2023-06-08 11:26:14 -0300

    feat: schools table added

--- sql/ba_paths.sql
+++ sql/ba_paths.sql
@@ -1,12 +1,20 @@
--- SENDEROS ------------------------------------------------------------------------------------------------------------
-CREATE TABLE paths(
-    id serial primary key,
-    path_id text,
+DROP TABLE  IF EXISTS schools CASCADE;
+CREATE TABLE schools(
+    school_id serial primary key,
+    cue numeric,
     name text,
     coordinates geometry,
-    neighborhood text references neighborhoods(neighborhood)
+    neighborhood text references neighborhoods(neighborhood),
+    CONSTRAINT school_cue UNIQUE (cue, name)
 );
 
+DROP TABLE IF EXISTS paths;
+CREATE TABLE paths(
+    path_id text,
+    school_id integer references schools(school_id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS aux_paths;
 CREATE TEMPORARY TABLE aux_paths(
     CUE numeric,
     name text,
@@ -24,8 +32,12 @@ CREATE TEMPORARY TABLE aux_paths(
 
 COPY aux_paths FROM '/Users/paudomingues/Documents/ITBA/OLAP/SecurityInsightsBA-OLAP/data/paths/senderos_escolares_2023.csv' DELIMITER ',' NULL AS '' CSV HEADER;
 
-INSERT INTO paths(path_id, name, coordinates, neighborhood)
-SELECT path_id, name, ST_POINT(long, lat), barrio
+INSERT INTO schools(cue, name, coordinates, neighborhood)
+SELECT cue, name, ST_POINT(long, lat), barrio
+FROM aux_paths;
+
+INSERT INTO paths
+SELECT path_id, (SELECT school_id FROM schools s WHERE aux_paths.CUE = s.cue AND aux_paths.name = s.name)
 FROM aux_paths;
 
 DROP TABLE aux_paths;
