c7bc4fbc47973014055d3e8b0937e41865153bfd
Paula Domingues <pdomingues@itba.edu.ar>
2023-06-17 23:03:21 -0300

    fix: neighborhoods added to subway stations table

--- sql/ba_paths.sql
+++ sql/ba_paths.sql
@@ -1,33 +1,36 @@
-DROP TABLE  IF EXISTS schools CASCADE;
-CREATE TABLE schools(
-    school_id serial primary key,
-    cue numeric,
-    name text,
-    coordinates geometry,
-    neighborhood text references neighborhoods(neighborhood),
+DROP TABLE IF EXISTS schools CASCADE;
+CREATE TABLE schools
+(
+    school_id    serial primary key,
+    cue          numeric,
+    name         text,
+    coordinates  geometry,
+    neighborhood text references neighborhoods (neighborhood),
     CONSTRAINT school_cue UNIQUE (cue, name)
 );
 
 DROP TABLE IF EXISTS paths;
-CREATE TABLE paths(
-    path_id text,
-    school_id integer references schools(school_id) ON DELETE CASCADE
+CREATE TABLE paths
+(
+    path_id   text,
+    school_id integer references schools (school_id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS aux_paths;
-CREATE TEMPORARY TABLE aux_paths(
-    CUE numeric,
-    name text,
+CREATE TEMPORARY TABLE aux_paths
+(
+    CUE     numeric,
+    name    text,
     address text,
     path_id text,
-    scope text,
-    barrio text,
-    comuna text,
-    de text,
-    X numeric,
-    Y numeric,
-    lat numeric,
-    long numeric
+    scope   text,
+    barrio  text,
+    comuna  text,
+    de      text,
+    X       numeric,
+    Y       numeric,
+    lat     numeric,
+    long    numeric
 );
 
 COPY aux_paths FROM '/Users/paudomingues/Documents/ITBA/OLAP/SecurityInsightsBA-OLAP/data/paths/senderos_escolares_2023.csv' DELIMITER ',' NULL AS '' CSV HEADER;
--- sql/datetime.sql
+++ sql/datetime.sql
@@ -1,28 +1,28 @@
 DROP TABLE IF EXISTS datetime;
 DROP TABLE if exists date;
 
-CREATE TABLE datetime (
-    date_key serial primary key,
-    time_range integer,
-    date date,
-    daynbmonth integer,
-    month integer,
-    month_name text,
-    daynbweek integer,
+CREATE TABLE datetime
+(
+    date_key    serial primary key,
+    time_range  integer,
+    date        date,
+    daynbmonth  integer,
+    month       integer,
+    month_name  text,
+    daynbweek   integer,
     daynameweek text,
-    is_weekend boolean,
-    year integer
+    is_weekend  boolean,
+    year        integer
 );
 
 INSERT INTO datetime (date, month, year, month_name, daynbweek, daynameweek, daynbmonth, is_weekend, time_range)
-SELECT
-    date_value,
-    EXTRACT(month FROM date_value),
-    EXTRACT(year FROM date_value),
-    to_char(date_value, 'Month'),
-    EXTRACT(isodow FROM date_value),
-    to_char(date_value, 'Day'),
-    EXTRACT(day FROM date_value),
-    EXTRACT(isodow FROM date_value) IN (6, 7), -- Check if day is Saturday (6) or Sunday (7)
-    generate_series(0, 23) -- Generate a series of numbers from 0 to 23
+SELECT date_value,
+       EXTRACT(month FROM date_value),
+       EXTRACT(year FROM date_value),
+       to_char(date_value, 'Month'),
+       EXTRACT(isodow FROM date_value),
+       to_char(date_value, 'Day'),
+       EXTRACT(day FROM date_value),
+       EXTRACT(isodow FROM date_value) IN (6, 7), -- Check if day is Saturday (6) or Sunday (7)
+       generate_series(0, 23)                     -- Generate a series of numbers from 0 to 23
 FROM generate_series('2016-01-01'::date, '2021-12-31'::date, '1 day') as date_value;
