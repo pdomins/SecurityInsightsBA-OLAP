9339380f2f23c51ba389e466b89c83d2c780b161
Paula Domingues <pdomingues@itba.edu.ar>
2023-06-19 19:10:45 -0300

    feat: sub-objectives 4 and 5

--- sql/queries.sql
+++ sql/queries.sql
@@ -92,7 +92,7 @@ WHERE ST_Within(c.coordinates, s.buffered_geometry) AND c.date_key = d.date_key
 GROUP BY station, area_square_meters, d.year, neighborhood, line)
 SELECT station, c.year, line, cs.crimes_per_square_meter - c.crimes_per_square_meter AS difference
 FROM crimes_per_station_and_year c, crime_statistics cs
-WHERE c.neighborhood = c.neighborhood AND c.year = cs.year
+WHERE c.neighborhood = cs.neighborhood AND c.year = cs.year
 ORDER BY difference DESC;
 
 -- 2.b
@@ -107,7 +107,7 @@ GROUP BY station, area_square_meters, d.year, neighborhood, line),
 crimes_comparison AS(
     SELECT station, c.year, line, cs.crimes_per_square_meter - c.crimes_per_square_meter AS difference
         FROM crimes_per_station_and_year c, crime_statistics cs
-        WHERE c.neighborhood = c.neighborhood AND c.year = cs.year
+        WHERE c.neighborhood = cs.neighborhood AND c.year = cs.year
 )
 SELECT line, year, AVG(difference) as average
 FROM crimes_comparison
@@ -190,3 +190,59 @@ average_crimes_amount AS (
 SELECT ms.month, ms.crimes_amount, ac.avg_crimes_amount - ms.crimes_amount AS difference
 FROM monthly_statistics ms
 CROSS JOIN average_crimes_amount ac;
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- crime patterns by hour of the day
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 1
+WITH crimes_by_hour AS (
+    SELECT d.time_range, neighborhood, COUNT(*) AS total_crimes
+    FROM crimes c, neighborhoods n, datetime d
+    WHERE c.date_key = d.date_key AND ST_Contains(n.multipolygon, c.coordinates)
+    GROUP BY d.time_range, n.neighborhood
+), average_crimes_by_neighborhood AS (
+SELECT neighborhood, AVG(total_crimes) AS avg_crimes
+FROM crimes_by_hour
+GROUP BY neighborhood
+)
+SELECT c.time_range, c.neighborhood, c.total_crimes
+FROM crimes_by_hour c
+JOIN average_crimes_by_neighborhood a ON c.neighborhood = a.neighborhood
+WHERE c.total_crimes > a.avg_crimes * 1.8
+ORDER BY c.neighborhood, c.time_range;
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- influence of police stations
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--1
+WITH distance_to_stations AS (
+    SELECT c.id, ps.name, ps.neighborhood, ST_Distance(c.coordinates, ps.coordinates) AS distance
+    FROM crimes c
+    CROSS JOIN LATERAL (
+        SELECT police_stations.name, police_stations.coordinates, police_stations.neighborhood
+        FROM police_stations
+        ORDER BY ST_Distance(c.coordinates, police_stations.coordinates)
+        LIMIT 1
+    ) AS ps)
+SELECT neighborhood, AVG(distance) AS distance
+FROM distance_to_stations
+GROUP BY neighborhood
+ORDER BY distance;
+--2
+WITH police_station_buffer AS (
+  SELECT name, neighborhood, ST_Buffer(coordinates, 0.00500) AS buffered_geometry, ST_area(ST_Buffer(coordinates, 0.00100))*10000*1000000 AS area_square_meters
+    FROM police_stations
+), crimes_per_station_and_year AS (
+SELECT p.name, d.year, neighborhood, COUNT(c.coordinates) AS crimes_count, COUNT(c.coordinates)/area_square_meters AS crimes_per_square_meter
+FROM crimes c, police_station_buffer p, datetime d
+WHERE ST_Within(c.coordinates, p.buffered_geometry) AND c.date_key = d.date_key
+GROUP BY p.name, area_square_meters, d.year, neighborhood),
+crimes_comparison AS(
+    SELECT name, c.neighborhood, c.year, cs.crimes_per_square_meter - c.crimes_per_square_meter AS difference
+        FROM crimes_per_station_and_year c, crime_statistics cs
+        WHERE c.neighborhood = cs.neighborhood AND c.year = cs.year
+)
+SELECT name, neighborhood, AVG(difference) as average
+FROM crimes_comparison
+GROUP BY name, neighborhood
+ORDER BY average DESC;
